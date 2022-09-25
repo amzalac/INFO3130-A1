e53ed1a7173298d044a42532aae82ee95c1c6e6d
amzalac <abbyzalac@gmail.com>
2022-09-24 23:36:26 -0400

    Nav menu + page animations completed

--- main.js
+++ main.js
@@ -1,8 +1,19 @@
 function leftContentClick() {
     document.getElementById('left-animation').classList.add('left-animation');
-
     let pageContent = document.getElementById("my-designs-page-content");
-    setTimeout(function(){ pageContent.style.display = "block"}, 2000);
+    setTimeout(function(){pageContent.style.display = "block"}, 2000);
+}
+
+function rightContentClick() {
+    document.getElementById('right-animation').classList.add('right-animation');
+    let pageContent = document.getElementById("about-me-page-content");
+    setTimeout(function(){pageContent.style.display = "block"}, 2000);
+}
+
+function bottomContentClick() {
+    document.getElementById('bottom-animation').classList.add('bottom-animation');
+    let pageContent = document.getElementById("contact-page-content");
+    setTimeout(function(){pageContent.style.display = "block"}, 2000);
 }
 
 
